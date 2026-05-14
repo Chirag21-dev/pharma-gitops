34cc54ebc4eadcfbb8f58a226de316147adb6222
Chirag21-dev <chiragks21@outlook.com>
2026-05-14 05:06:09 +0000

    Added the resource block for ecr,parent module.

--- Terrraform/Env/dev/main.tf
+++ Terrraform/Env/dev/main.tf
@@ -10,3 +10,16 @@ module "vpc" {
   private_eks_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
   private_rds_subnet_cidrs = ["10.0.5.0/24", "10.0.6.0/24"]
 }
+
+module "ecr" {
+  source = "../../modules/ecr"
+
+  project = "pharma"
+  env     = "dev"
+  repositories = [
+    
+    "pharma-ui",
+    "supplier-service",
+    
+  ]
+}
